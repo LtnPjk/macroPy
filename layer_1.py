1ecc6be348c47dbb5623dbe603ba7c1afdb6855a
LtnPjk <olle.olofsson97@gmail.com>
2019-01-15 09:18:11 +0100

    Reusable macro methods added

--- layer_1.py
+++ layer_1.py
@@ -6,8 +6,7 @@ from pynput.keyboard import Key, Controller
 import pyperclip
 import pyautogui
 
-#This is where you define your macros
-def macro1():
+def copyTofile(filename):
     #Read current clipboard to restore later
     oldCb = pyperclip.paste()
     keyboard = Controller()
@@ -15,43 +14,47 @@ def macro1():
     pyautogui.hotkey('ctrl', 'c')
     newCb = str(pyperclip.paste())
     #Save new clipboard to file '0_1_clp.txt'
-    file = open(os.path.dirname(os.path.realpath(__file__))+'/0_1_clp.txt', 'w')
+    file = open(os.path.dirname(os.path.realpath(__file__))+'/'+filename, 'w')
     file.write(newCb)
     file.close()
     print("Copied:\n'", newCb, "'")
     #Restore old clipboard
     pyperclip.copy(oldCb)
 
+#This is where you define your macros
+def macro1():
+    copyTofile('0_1_clp.txt')
+    
 def macro2():
-    print('macro2')
+    copyTofile('0_2_clp.txt')
 
 def macro3():
-    print('macro3')
+    copyTofile('0_3_clp.txt')
 
 def macro4():
-    print('macro4')
+    copyTofile('0_4_clp.txt')
 
 def macro5():
-    print('macro5')
+    copyTofile('0_5_clp.txt')
 
 def macro6():
-    print('macro6')
+    copyTofile('0_6_clp.txt')
 
 def macro7():
-    print('macro7')
+    copyTofile('0_7_clp.txt')
 
 def macro8():
-    print('macro8')
+    copyTofile('0_8_clp.txt')
 
 def macro9():
-    print('macro9')
+    copyTofile('0_9_clp.txt')
 
 def macro10():
-    print('macro10')
+    copyTofile('0_10_clp.txt')
 
 def macro11():
-    print('macro11')
+    copyTofile('0_11_clp.txt')
 
 def macro12():
-    print('macro12')
+    copyTofile('0_12_clp.txt')
 
--- layer_5.py
+++ layer_5.py
@@ -5,48 +5,53 @@ import io
 import pyautogui
 import pyperclip
 
-#This is where you define your layers
+def pasteFromFile(filename):
+    try:
+        #Read content of '0_1_clp.txt' and paste
+        oldCb = pyperclip.paste()
+        file = open(os.path.dirname(os.path.realpath(__file__))+'/'+filename, 'r')
+        newCb = file.read()
+        file.close()
+        pyperclip.copy(newCb)
+        pyautogui.hotkey('ctrl', 'v')
+        print('Pasted: ', newCb)
+        pyperclip.copy(oldCb)
+    except:
+        print('FILE NOT FOUND!')
 
+#This is where you define your layers
 def macro1():
-    #Read content of '0_1_clp.txt' and paste
-    oldCb = pyperclip.paste()
-    file = open(os.path.dirname(os.path.realpath(__file__))+'/0_1_clp.txt', 'r')
-    newCb = file.read()
-    file.close()
-    pyperclip.copy(newCb)
-    pyautogui.hotkey('ctrl', 'v')
-    print("Pasted:\n'", newCb,"'")
-    pyperclip.copy(oldCb)
+    pasteFromFile('0_1_clp.txt')
 
 def macro2():
-    print('macro2')
+    pasteFromFile('0_2_clp.txt')
 
 def macro3():
-    print('macro3')
+    pasteFromFile('0_3_clp.txt')
 
 def macro4():
-    print('macro4')
+    pasteFromFile('0_4_clp.txt')
 
 def macro5():
-    print('macro5')
+    pasteFromFile('0_5_clp.txt')
 
 def macro6():
-    print('macro6')
+    pasteFromFile('0_6_clp.txt')
 
 def macro7():
-    print('macro7')
+    pasteFromFile('0_7_clp.txt')
 
 def macro8():
-    print('macro8')
+    pasteFromFile('0_8_clp.txt')
 
 def macro9():
-    print('macro9')
+    pasteFromFile('0_9_clp.txt')
 
 def macro10():
-    print('macro10')
+    pasteFromFile('0_10_clp.txt')
 
 def macro11():
-    print('macro11')
+    pasteFromFile('0_11_clp.txt')
 
 def macro12():
-    print('macro12')
+    pasteFromFile('0_12_clp.txt')
